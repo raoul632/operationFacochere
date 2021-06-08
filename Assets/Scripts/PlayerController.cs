8e296e2b4c6e3ed94634b8a2dc3d5e6e55f365bb
raoul <sshpcl2@gmail.com>
2021-06-08 20:00:12 +0200

    mise en place de deux animation idle et walk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Transform handGun;
     
-    private Camera _cam; 
+    private Camera _cam;
+    private Animator _animator;
+    private Vector2 moveInput ;
+ 
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>(); 
         _cam = Camera.main; 
     }
 
@@ -22,21 +26,25 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        float moveVertical = Input.GetAxis("Vertical") * speed ;
-        float moveHorizontal = Input.GetAxis("Horizontal") * speed ;
+
+        moveInput.y = Input.GetAxis("Vertical") ;
+        moveInput.x = Input.GetAxis("Horizontal") ;
+
+        rb.velocity = moveInput * speed;
+        print(rb.velocity);
 
 
         Vector3 mousePosition = Input.mousePosition;
         Vector3 playerPosition = _cam.WorldToScreenPoint(transform.position);
 
-        if(mousePosition.x < playerPosition.x){
-            transform.localScale = new Vector3(-1f, 1f, 1f); 
-            handGun.localScale  = new Vector3(-1f, -1f, 1f); 
+        if (mousePosition.x < playerPosition.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            handGun.localScale = new Vector3(-1f, -1f, 1f);
         }
         else
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
-
             handGun.localScale = Vector3.one;
         }
 
@@ -46,13 +54,20 @@ public class PlayerController : MonoBehaviour
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        handGun.rotation = Quaternion.Euler(0, 0, angle); 
+        handGun.rotation = Quaternion.Euler(0, 0, angle);
 
-       
-    //transform.Translate(moveHorizontal, moveVertical, 0);  <-- applique le mouvement au transform pas a la physique
-        rb.velocity = new Vector2( moveHorizontal, moveVertical);
-       
-       
+
+        //transform.Translate(moveHorizontal, moveVertical, 0);  <-- applique le mouvement au transform pas a la physique
+        if (rb.velocity != Vector2.zero)
+        {
+            _animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            _animator.SetBool("IsMoving", false);
+        }
+
+
 
     }
     
